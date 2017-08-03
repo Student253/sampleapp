909e20d34bececef7f5c4b080c701145b927eb0b
Ali Hassan <ranaalihasssan253@gmail.com>
2017-08-03 15:53:01 +0500

    This is first comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { trigger,state,style,transition, animate,keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: 
+  `
+  	<p [@myAnimation]= 'state' (click)= "animatePara()"> Hello! Click me</p>
+
+  `,
+  styles: [`
+  p {
+  	width:400px;
+  	margin: 200px auto;
+  	font-size: 6em;
+  }
+
+  `],
+  animations : [
+
+  trigger('myAnimation', [
+  	state ('little', style({
+  		transform: 'scale(1)',
+  	})),
+
+  	state ('large', style({
+  		transform: 'scale(1.2)',
+  	})),
+  	transition ('little <=> large', animate('1500ms ease-in')),
+
+  	]),
+
+  ]
 })
 export class AppComponent {
   title = 'app';
+
+  state: string = 'little';
+
+  animatePara() {
+  	this.state = (this.state === 'little' ? 'large': 'little');
+  }
 }
